5f917891937d19a0e3e988471ae88658b2a383e9
Artur Dani <darthur83@hotmail.com>
2022-02-25 13:01:25 +0100

    refactore folder structure to use src

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
